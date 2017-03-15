45befbfb8c5c0a3485db31c49b6e0c177b8a71c0
t-vick <t-vick@msn.com>
2017-03-15 10:36:08 +0800

    清理app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
-import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { GarageModule } from './garage/garage.module';
-import 'hammerjs';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LayoutComponent,
-    SidebarComponent,
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    MaterialModule,
-    FlexLayoutModule,
-    AppRoutingModule,
     GarageModule,
     CoreModule,
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@angular/material';
+import { AppRoutingModule } from '../app-routing.module';
+
 import { ConfigService } from './config.service';
+import { LayoutComponent } from './layout/layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-	imports: [],
-	declarations: [],
-	exports: [],
+	imports: [ FlexLayoutModule, MaterialModule, AppRoutingModule ],
+	declarations: [ LayoutComponent, SidebarComponent ],
+	exports: [ LayoutComponent ],
 	providers: [ ConfigService ],
 })
 export class CoreModule {}
