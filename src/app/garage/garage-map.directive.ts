67cf3f57750d722200fbaf1f490eda06757aeb6d
unknown <>
2017-03-15 21:48:04 +0800

    画图指令移动图像已实现

--- src/app/garage/garage-map.directive.ts
+++ src/app/garage/garage-map.directive.ts
@@ -9,14 +9,11 @@ export class GarageMapDirective implements OnInit{
 	private mapOption: Object;
 	private ctx: any;
 	private isMouseDown: boolean = false;
-	private mouseDownX: number = 0;
-	private mouseDownY: number = 0;
-	private test = {
-		x:0,
-		y:0,
-		xLoc:0,
-		yLoc:0
-	}
+	private oldX: number = 0;
+	private oldY: number = 0;
+	private offsetX: number = 0;
+	private offsetY: number = 0;
+
 	constructor(private el: ElementRef){
 		this.ctx=el.nativeElement.getContext("2d");
 		this.ctx.lineCap = 'round';
@@ -37,31 +34,24 @@ export class GarageMapDirective implements OnInit{
 	}
 
 	@HostListener('mousedown', ['$event']) onMouseDown(ev) {
-		this.mouseDownX = ev.clientX;
-		this.mouseDownY = ev.clientY;
+		this.oldX = ev.clientX;
+		this.oldY = ev.clientY;
 		this.isMouseDown = true;
 	}
 
 	@HostListener('mousemove', ['$event']) onMouseMove(ev) {
 		if (this.isMouseDown) {
-			this.test.x = this.mouseDownX;
-			this.test.y = this.mouseDownY;
-			this.test.xLoc += ev.clientX - this.test.x;
-			this.test.yLoc += ev.clientY - this.test.y;
-			this.ctx.translate(this.test.xLoc/1000, this.test.yLoc/1000);
-			this.ctx.clearRect(0,0,400,300);
+			this.offsetX = ev.clientX - this.oldX;
+			this.offsetY = ev.clientY - this.oldY;
+			this.oldX = ev.clientX;
+			this.oldY = ev.clientY;
+			this.ctx.translate(this.offsetX, this.offsetY);
+			this.ctx.clearRect(-this.offsetX,-this.offsetY,400,300);//清空画布
 			this.ctx.fillRect(0,0,150,75);
 		}
-		// console.log(ev);
-		// console.log(this.mapOption);
 	}
 
 	@HostListener('mouseup') onMouseUp() {
 		this.isMouseDown = false;
-	}
-
-	@HostListener('mousedrag', ['$event']) onDragStart(ev) {
-		this.test.x = ev.clientX;
-		console.log(ev);
 	}
 }
