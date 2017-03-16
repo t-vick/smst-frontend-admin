b578147e7245b9915f72595922ce438280586dbe
unknown <>
2017-03-17 00:33:09 +0800

    新增图形旋转，优化清理画布，清理后找回清理前的原点偏移量

--- src/app/garage/garage-map.directive.ts
+++ src/app/garage/garage-map.directive.ts
@@ -10,9 +10,7 @@ export class GarageMapDirective implements AfterViewInit, OnChanges {
 	private mapOption: Object;
 	private ctx: any;
 	private isMouseDown: boolean = false;
-	private oldX: number = 0;
-	private oldY: number = 0;
-	private offsetX: number = 0;
+	private offsetX: number = 0;//绘图区域左上角，相对于初始位置的偏移量
 	private offsetY: number = 0;
 
 	constructor(private el: ElementRef){
@@ -22,13 +20,23 @@ export class GarageMapDirective implements AfterViewInit, OnChanges {
 	private resize() {
 		this.ctx.canvas.setAttribute('height',this.el.nativeElement.clientHeight);//必须先设置高度？
 		this.ctx.canvas.setAttribute('width',this.el.nativeElement.clientWidth);
-
+		//调整大小后，要把原点移动到调整大小前的位置
+		//否则清空绘图区的时候，有的地方会清理不到
+		//因为，此时绘图区域与清理区域没有完全重合
+		this.ctx.translate(this.offsetX,this.offsetY);
 		
 		this.ctx.lineCap = 'round';
 		this.ctx.lineJoin = 'round';
 		this.ctx.globalAlpha = 0.3;
 		this.ctx.fillStyle = '#ff0000';
 		this.ctx.fillRect(0,0,75,75);
+		this.ctx.save();
+		this.ctx.translate(37.5, 37.5);
+		this.ctx.rotate(Math.PI/4);
+		this.ctx.translate(-37.5, -37.5);
+		this.ctx.fillRect(0,0,75,75);
+		this.ctx.restore();
+		console.log(this.el);
 	}
 
 	ngAfterViewInit() {
@@ -42,21 +50,24 @@ export class GarageMapDirective implements AfterViewInit, OnChanges {
 	}
 
 	@HostListener('mousedown', ['$event']) onMouseDown(ev) {
-		this.oldX = ev.layerX;
-		this.oldY = ev.layerY;
 		this.isMouseDown = true;
 	}
 
 	@HostListener('mousemove', ['$event']) onMouseMove(ev) {
 		if (this.isMouseDown) {
-			// this.offsetX = ev.layerX - this.oldX;
-			// this.offsetY = ev.layerY - this.oldY;
-			// this.oldX = ev.layerX;
-			// this.oldY = ev.layerY;
-			// this.ctx.translate(this.offsetX, this.offsetY);//调节鼠标灵敏度，3.7时图像刚好跟随鼠标动作
+			this.offsetX += ev.movementX;
+			this.offsetY += ev.movementY;
 			this.ctx.translate(ev.movementX, ev.movementY);
-			this.ctx.clearRect(-ev.movementX,-ev.movementY,this.el.nativeElement.clientWidth,this.el.nativeElement.clientHeight);//清空画布及其周围100像素以内的元素
+			this.ctx.clearRect(-this.offsetX,-this.offsetY,this.el.nativeElement.clientWidth,this.el.nativeElement.clientHeight);
+
 			this.ctx.fillRect(0,0,75,75);
+
+			this.ctx.save();
+			this.ctx.translate(37.5, 37.5);
+			this.ctx.rotate(Math.PI/4);
+			this.ctx.translate(-37.5, -37.5);
+			this.ctx.fillRect(0,0,75,75);
+			this.ctx.restore();
 		}
 	}
 
