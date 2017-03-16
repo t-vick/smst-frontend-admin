db61dae0b7f1c32f50e521aa6f4ab513dd3e7549
t-vick <t-vick@msn.com>
2017-03-16 14:28:56 +0800

    绘图指令成功跟随鼠标轨迹

--- src/app/garage/garage-map.directive.ts
+++ src/app/garage/garage-map.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, AfterViewInit } from '@angular/core';
 
 @Directive({
 	selector: '[garageMap]',
 
 })
-export class GarageMapDirective implements OnInit{
+export class GarageMapDirective implements AfterViewInit{
 	@Input('garageMap')
 	private mapOption: Object;
 	private ctx: any;
@@ -15,41 +15,39 @@ export class GarageMapDirective implements OnInit{
 	private offsetY: number = 0;
 
 	constructor(private el: ElementRef){
-		this.ctx=el.nativeElement.getContext("2d");
+		this.ctx=this.el.nativeElement.getContext("2d");
+	};
+
+	ngAfterViewInit() {
+		this.ctx.canvas.setAttribute('height',this.el.nativeElement.clientHeight);//必须先设置高度？
+		this.ctx.canvas.setAttribute('width',this.el.nativeElement.clientWidth);
+
+		
 		this.ctx.lineCap = 'round';
 		this.ctx.lineJoin = 'round';
 		this.ctx.globalAlpha = 0.3;
 
 		this.ctx.fillStyle = '#ff0000';
 		this.ctx.fillRect(0,0,75,75);
-		el.nativeElement.addEventListener('ondragstart',function(ev){
-			console.log(ev);
-		})
-		console.log(this.ctx);
-		console.log(el);
-	};
-
-	ngOnInit() {
-		console.log(this.mapOption);
 	}
 
 	@HostListener('mousedown', ['$event']) onMouseDown(ev) {
-		this.oldX = ev.clientX;
-		this.oldY = ev.clientY;
+		this.oldX = ev.layerX;
+		this.oldY = ev.layerY;
 		this.isMouseDown = true;
 	}
 
 	@HostListener('mousemove', ['$event']) onMouseMove(ev) {
 		if (this.isMouseDown) {
-			this.offsetX = ev.clientX - this.oldX;
-			this.offsetY = ev.clientY - this.oldY;
-			this.oldX = ev.clientX;
-			this.oldY = ev.clientY;
-			this.ctx.translate(this.offsetX/3.7, this.offsetY/3.7);//调节鼠标灵敏度，3.7时图像刚好跟随鼠标动作
-			this.ctx.clearRect(-this.offsetX-100,-this.offsetY-100,this.el.nativeElement.clientWidth + 100,this.el.nativeElement.clientHeight+100);//清空画布及其周围100像素以内的元素
+			// this.offsetX = ev.layerX - this.oldX;
+			// this.offsetY = ev.layerY - this.oldY;
+			// this.oldX = ev.layerX;
+			// this.oldY = ev.layerY;
+			// this.ctx.translate(this.offsetX, this.offsetY);//调节鼠标灵敏度，3.7时图像刚好跟随鼠标动作
+			this.ctx.translate(ev.movementX, ev.movementY);
+			this.ctx.clearRect(-ev.movementX,-ev.movementY,this.el.nativeElement.clientWidth,this.el.nativeElement.clientHeight);//清空画布及其周围100像素以内的元素
 			this.ctx.fillRect(0,0,75,75);
 		}
-		console.log(this.ctx);
 	}
 
 	@HostListener('mouseup') onMouseUp() {
