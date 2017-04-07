5be9df9a84ae994972423f67b9c3fa41a48ff8c0
t-vick <t-vick@msn.com>
2017-04-07 16:39:21 +0800

    实现数据库访问获取数据

--- src/app/garage/garage-map/garage-map.component.ts
+++ src/app/garage/garage-map/garage-map.component.ts
@@ -12,53 +12,61 @@ import { GarageMapService } from './garage-map.service';
 	styleUrls: [ './garage-map.component.css' ],
 })
 export class GarageMapComponent implements OnInit, DoCheck, AfterViewInit {
-  private option: any;
+    private option: any;
 
-  private boxSize: number;
-  constructor(
-    private garageMapService: GarageMapService, 
-    private el: ElementRef, 
-    private dialog: MdDialog) {
+    private boxSize: number;
+    constructor(
+        private garageMapService: GarageMapService, 
+        private el: ElementRef, 
+        private dialog: MdDialog) {
   	
-  }
-
-  ngOnInit() {
-    // this.garageMapService.getRoads()
-    // .subscribe(
-    //     (data) => {
-    //         console.log(data[0] instanceof Road);
-    //         let list = new Road(data[0]);
-    //         console.log(list instanceof Road);
-    //     }
-    //   );
-    this.option = {
-      cellList: [
-        new Cell({x: 150, y: 150, w: 75, h: 75, rx: 187.5, ry: 187.5, deg: 0}),
-
-        new Cell({x: 0, y: 0, w: 75, h: 75, rx: 37.5,ry: 37.5, deg: 0.25})
-      ],
-      roadList: [
-        new Road({x: -10, y: 250, ex: 400, ey: 250, lw: 20, rx: 195, ry: 250, hl: 205, deg: 0}),
-        new Road({x: -10, y: 400, ex: 400, ey: 400, lw: 20, rx: 195, ry: 400, hl: 205, deg: 0}),
-        new Road({x: 30, y: -10, ex: 30, ey: 800, lw: 20, rx: 30, ry: 395, hl: 405, deg: 0.5}),
-        new Road({x: 330, y: -10, ex: 330, ey: 800, lw: 20, rx: 330, ry: 395, hl: 405, deg: 0.5}),
-      ],
     }
-  }
 
-  ngDoCheck() {
-  	if (!this.boxSize) return;
-  	this.boxSize = this.el.nativeElement.clientWidth * this.el.nativeElement.clientHeight;
-  }
+    ngOnInit() {
+        this.option = new Object();
+        this.option.roadList = new Array<Road>();
+        this.option.cellList = new Array<Cell>();
+        this.garageMapService.getRoads(1)
+        .subscribe(
+            (data) => {
+                for (let road  of data.floors[0].roads) {
+                  this.option.roadList.push(new Road(road));
+                }
 
-  ngAfterViewInit() {
-  	this.boxSize = this.el.nativeElement.clientWidth * this.el.nativeElement.clientHeight;
-  }
+                for (let cell of data.floors[0].cells) {
+                  this.option.cellList.push(new Cell(cell));
+                }
+            }
+        );
 
-  openDialog() {
-    let dialogRef = this.dialog.open(GarageObjAddComponent);
-    dialogRef.afterClosed().subscribe(result => {
+        // this.option = {
+        //     cellList: [
+        //         new Cell({x: 150, y: 150, w: 75, h: 75, rx: 187.5, ry: 187.5, deg: 0}),
 
-    });
-  }
+        //         new Cell({x: 0, y: 0, w: 75, h: 75, rx: 37.5,ry: 37.5, deg: 0.25})
+        //     ],
+        //     roadList: [
+        //         new Road({x: -10, y: 250, ex: 400, ey: 250, lw: 20, rx: 195, ry: 250, hl: 205, deg: 0}),
+        //         new Road({x: -10, y: 400, ex: 400, ey: 400, lw: 20, rx: 195, ry: 400, hl: 205, deg: 0}),
+        //         new Road({x: 30, y: -10, ex: 30, ey: 800, lw: 20, rx: 30, ry: 395, hl: 405, deg: 0.5}),
+        //         new Road({x: 330, y: -10, ex: 330, ey: 800, lw: 20, rx: 330, ry: 395, hl: 405, deg: 0.5}),
+        //     ],
+        // }
+    }
+
+    ngDoCheck() {
+        if (!this.boxSize) return;
+  	    this.boxSize = this.el.nativeElement.clientWidth * this.el.nativeElement.clientHeight;
+    }
+
+    ngAfterViewInit() {
+  	    this.boxSize = this.el.nativeElement.clientWidth * this.el.nativeElement.clientHeight;
+    }
+
+    openDialog() {
+        let dialogRef = this.dialog.open(GarageObjAddComponent);
+        dialogRef.afterClosed().subscribe(result => {
+
+        });
+    }
 }
--- src/app/garage/garage-map/garage-map.service.ts
+++ src/app/garage/garage-map/garage-map.service.ts
@@ -4,11 +4,11 @@ import { AppConfigService } from '../../app-config.service';
 import { Subject } from 'rxjs/Subject';
 @Injectable()
 export class GarageMapService {
-	private url: string = '/roads';
+	private url: string = '/maps';
 	public subject: Subject<any> = new Subject<any>();
 	constructor(private http: Http, private appConfigService: AppConfigService){}
-	public getRoads() {
-		return this.http.get(this.appConfigService.baseUrl + this.url)
+	public getRoads(id: number) {
+		return this.http.get(this.appConfigService.baseUrl + this.url + '/' + id)
 						.map((response: Response) => {
 							return response.json();
 							// let roads = response.json();
